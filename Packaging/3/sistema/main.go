11f77e11acd8154c287452e7e6106fceba9ce19b
arthur-ngdi <arthur.f@gec.inatel.br>
2026-04-30 13:58:28 -0300

    correcting version related files of Packaging Part 3

---
+++ Packaging/3/sistema/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/arthur-ngdi/curso-go-expert/Packaging/3/math"
+
+func main() {
+	m := math.NewMath(1, 2)
+	println(m.Add())
+}
--- Packaging/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/arthur-ngdi/curso-go-expert/Packaging/2
-
-go 1.24.4
-
-require github.com/google/uuid v1.6.0
